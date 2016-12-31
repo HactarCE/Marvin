97fa7f67c5c5f9175a19fee20ec85caa15487521
Hactar <legomany3448@gmail.com>
2016-12-31 17:09:00 -0500

    Indent with tabs

--- src/hactarce/marvin/Main.java
+++ src/hactarce/marvin/Main.java
@@ -2,7 +2,8 @@ package hactarce.marvin;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // write your code here
-    }
+	public static void main(String[] args) {
+		// write your code here
+	}
+
 }
